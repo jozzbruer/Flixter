b991bd15a3f3f4c27d95ff170b80949126a47a9f
Joz-Bruer Quince <quincejozbruer@gmail.com>
2022-02-12 14:01:52 -0800

    Fix the navigation controller and fetch data from the API

--- Flixter/MovieViewController.swift
+++ Flixter/MovieViewController.swift
@@ -11,8 +11,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-      
+        // Get the data from the movie database API
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=d443226de816e358594a7fa4c7384b2b")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+             // This will run when the network request returns
+             if let error = error {
+                    print(error.localizedDescription)
+             } else if let data = data {
+                    let finalData = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+
+                    // TODO: Get the array of movies
+                 print(finalData)
+                    // TODO: Store the movies in a property to use elsewhere
+                    // TODO: Reload your table view data
+
+             }
+        }
+        task.resume()
     }
 
 
