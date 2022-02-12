bf49b23a67677613d1bbc6cf7176fc42de963542
Joz-Bruer Quince <quincejozbruer@gmail.com>
2022-02-12 14:42:16 -0800

    Create table view and add data into it

--- Flixter/MovieViewController.swift
+++ Flixter/MovieViewController.swift
@@ -7,10 +7,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+   
+    @IBOutlet weak var tableView: UITableView!
+    var movies = [[String: Any]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
         // Get the data from the movie database API
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=d443226de816e358594a7fa4c7384b2b")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -21,9 +27,10 @@ class ViewController: UIViewController {
                     print(error.localizedDescription)
              } else if let data = data {
                     let finalData = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-
+                    
+                 self.movies = finalData["results"] as! [[String: Any]]
+                 self.tableView.reloadData()
                     // TODO: Get the array of movies
-                 print(finalData)
                     // TODO: Store the movies in a property to use elsewhere
                     // TODO: Reload your table view data
 
@@ -31,7 +38,20 @@ class ViewController: UIViewController {
         }
         task.resume()
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cells = UITableViewCell()
+        
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        
+        cells.textLabel?.text = title
+        
+        return cells
+    }
 
 }
 
